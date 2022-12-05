a4fa1d22297f3404edca3e5b7d7ae03d8d2721bf
Mirkopoj <mirkopoj@hotmail.com>
2022-12-05 12:43:16 -0300

    run user cambio

--- sspa_install.sh
+++ sspa_install.sh
@@ -12,9 +12,9 @@ git clone https://github.com/Mirkopoj/sspa.git /opt/sspa
 
 if ! command -v rustup &> /dev/null
 then
-	runuser -l  $(logname) -c "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh"
+	runuser -u $(logname) -- curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
 
-runuser -l  $(logname) -c 'rustup update'
+runuser -l $(logname) -c 'rustup update'
 wget https://raw.githubusercontent.com/Mirkopoj/sspa_installer_script/master/sspa_uninstall.sh -o /bin/sspa_uninstall.sh
 chmod +x /bin/sspa_uninstall.sh
