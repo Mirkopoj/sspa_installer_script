601f9c2eb67370e83d8b6fe2ce8f581dd73fa7ce
Mirkopoj <mirkopoj@hotmail.com>
2022-12-05 16:55:01 -0300

    Installador funcional falta un test

---
+++ sspa_uninstall.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo rm /bin/sspa
+sudo rm /opt/sspa -r
+sudo groupdel sspa
+sudo rm /bin/sspa_uninstall.sh
--- sspa_unintall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-rm /bin/sspa
-rm /opt/sspa -r
-rm /bin/sspa_uninstall.sh
