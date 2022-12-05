1ce08a9e4e615e902f6b6766608f595c940fb0ed
Mirkopoj <mirkopoj@hotmail.com>
2022-12-05 12:17:36 -0300

    Rust install user

--- sspa_install.sh
+++ sspa_install.sh
@@ -8,7 +8,7 @@ git clone https://github.com/Mirkopoj/sspa.git /opt/sspa
 
 if ! command -v rustup &> /dev/null
 then
-	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+	runuser -l  $(logname) -c "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh"
 fi
 
 runuser -l  $(logname) -c 'rustup update'
