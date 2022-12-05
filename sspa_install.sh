e3b6c6d2812fabac4f78e30daeab9a89fc54b5ce
Mirkopoj <mirkopoj@hotmail.com>
2022-12-05 11:34:40 -0300

    Test 1

--- sspa_install.sh
+++ sspa_install.sh
@@ -2,3 +2,9 @@
 
 apt update && apt-get install git
 git clone https://github.com/Mirkopoj/sspa.git /opt/sspa
+if ! command -v rustup &> /dev/null
+then
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
+
+runuser -l  $(logname) -c 'rustup update'
