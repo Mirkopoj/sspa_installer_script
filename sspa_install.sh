6d7c14991ed8311f4e3aff29b3c0f1c8589805f0
Mirkopoj <mirkopoj@hotmail.com>
2022-12-05 12:13:20 -0300

    Rust path

--- sspa_install.sh
+++ sspa_install.sh
@@ -9,8 +9,9 @@ git clone https://github.com/Mirkopoj/sspa.git /opt/sspa
 if ! command -v rustup &> /dev/null
 then
 	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+	export PATH="$HOME/.cargo/bin:$PATH"
 fi
 
 runuser -l  $(logname) -c 'rustup update'
-wget https://raw.githubusercontent.com/Mirkopoj/sspa_installer_script/master/sspa_uninstall.sh -o /bin/sspa_uninstall
+wget https://raw.githubusercontent.com/Mirkopoj/sspa_installer_script/master/sspa_uninstall.sh -o /bin/sspa_uninstall.sh
 chmod +x /bin/sspa_uninstall.sh
