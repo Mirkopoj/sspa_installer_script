3c18d329585c0b8ca40c2d517db2d6c2dcd83ae9
Mirkopoj <mirkopoj@hotmail.com>
2022-12-05 11:45:37 -0300

    Installar unistall

--- sspa_install.sh
+++ sspa_install.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-apt update && apt-get install git
+if ! command -v git &> /dev/null
+then
+	apt update && apt-get install git
+fi
 git clone https://github.com/Mirkopoj/sspa.git /opt/sspa
+
 if ! command -v rustup &> /dev/null
 then
 	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
 
 runuser -l  $(logname) -c 'rustup update'
+wget https://raw.githubusercontent.com/Mirkopoj/sspa_installer_script/master/sspa_uninstall.sh -o /bin/sspa_uninstall
+chmod +x /bin/sspa_uninstall.sh
